5b8c7e42fa1a12b734f8d6387374b768e611b31b
victorvegaward <victorandresvega@gmail.com>
2022-04-05 09:26:52 -0400

    Add Item feature added

--- item.py
+++ item.py
@@ -21,7 +21,7 @@ class Item:
         self.style = self.valid_string_length(self.valid_string_type(style), 1, 16)
         self.gender = self.valid_gender_value(self.valid_string_type(gender))
         self.description = self.valid_string_length(self.valid_string_type(description), 1, 150)
-        self.image = self.valid_string_length(self.valid_string_type(image), 1, 150)
+        self.image = self.valid_string_length(self.valid_string_type(image), 1, 500)
         self.username = self.valid_user_type(user).username # to avoid testing if the username is real
 
     @staticmethod
